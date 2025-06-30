496e4920d706f7ee64e821d6d59f2de5e8d89189
Вадим <159821591+Shaitan406@users.noreply.github.com>
2025-06-30 16:19:00 +0300

    Update BonusProgramPage.java

--- 1.6_extended_tests/src/test/java/intersShop/pages/BonusProgramPage.java
+++ 1.6_extended_tests/src/test/java/intersShop/pages/BonusProgramPage.java
@@ -5,8 +5,7 @@
     import org.openqa.selenium.support.FindBy;
     import org.openqa.selenium.support.PageFactory;
 
-    public class BonusProgramPage extends Page
-    {
+    public class BonusProgramPage extends Page {
         private final String subUrl = "bonus/";
 
         @FindBy(css = "#bonus_main h3") //сообщение - карта оформлена
@@ -28,12 +27,10 @@
         public Boolean isSuccessResultMessageDisplayed() {
             return successResultMessage.isDisplayed();
         }
-        public boolean getError()
-        {
+        public boolean getError() {
             return errorNameErrorPhone.isDisplayed();
         }
-        public String getSubTitle()
-        {
+        public String getSubTitle() {
             return subTitle.getText();
         }
     }
